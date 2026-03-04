59970bc38a256481813d9c73bc64eda1c6556380
Asri <asri>
2026-03-04 18:28:37 +0530

    terraform modules

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -6,11 +6,10 @@ resource "aws_instance" "mongodb" {
 }
 
 resource "terraform_data" "mongodb" {
-
   triggers_replace = [
     aws_instance.mongodb.id
   ]
-
+  
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -26,11 +25,45 @@ resource "terraform_data" "mongodb" {
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/bootstrap.sh",
-      # "sudo sh /tmp/bootstrap.sh"
-      "sudo sh /tmp/bootstrap.sh mongodb",
-      "echo Hello world"
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mongodb"
     ]
   }
+}
 
+#redis
+
+resource "aws_instance" "redis" {
+  ami                    = "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.reddis_security_group_id]
+  subnet_id              = local.database_subnetsg
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
 }
