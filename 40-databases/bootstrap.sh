6b7b33cea427f703e5ee181ba8042081667372c9
Asri <asri>
2026-03-04 17:01:50 +0530

    terraform modules

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-set -e
-
-component=$1
-
-dnf makecache
 dnf install ansible -y
-
-ansible-pull \
--U https://github.com/daws-86s/ansible-roboshop-roles-tf.git \
--e component=$component \
-main.yaml
+#ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
