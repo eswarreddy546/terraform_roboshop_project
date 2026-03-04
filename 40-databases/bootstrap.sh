9c5ab0e0f4780b0d1a898e0e986988b7d2119acc
Asri <asri>
2026-03-04 16:22:35 +0530

    terraform modules

---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+component=$1
+dnf install ansible -y
+ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -18,8 +18,17 @@ resource "terraform_data" "mongodb" {
     host     = aws_instance.mongodb.private_ip
   }
 
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
   provisioner "remote-exec" {
     inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      # "sudo sh /tmp/bootstrap.sh"
+      "sudo sh /tmp/bootstrap.sh mongodb",
       "echo Hello world"
     ]
   }
