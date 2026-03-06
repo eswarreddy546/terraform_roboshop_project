2991cd89ee591d206dd56f5767dd81bad2a6cf06
Asri <asri>
2026-03-06 19:14:56 +0530

    roboshop modules

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -85,3 +85,48 @@ resource "aws_route53_record" "redis" {
   records = [aws_instance.redis.private_ip]
   allow_overwrite = true
 }
+
+#rabbitmq
+
+resource "aws_instance" "rabbitmq" {
+  ami                    = "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.reddis_security_group_id]
+  subnet_id              = local.database_subnetsg
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}" # rabbitmq-dev.daws86s.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+}
