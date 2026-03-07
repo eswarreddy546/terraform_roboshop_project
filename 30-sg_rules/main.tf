81e681a27a8d169087bc541db4cb39cc45ee1616
Asri <asri>
2026-03-07 10:00:11 +0530

    roboshop modules

--- 30-sg_rules/main.tf
+++ 30-sg_rules/main.tf
@@ -90,41 +90,41 @@ resource "aws_security_group_rule" "mysql_egress" {
 }
 
 
-# #catalogue
+#catalogue
 
-# resource "aws_security_group_rule" "catalogue" {
-#   type              = "ingress"
-#   security_group_id = local.catalogue_security_group_id
-#   source_security_group_id = local.bastion_sg_id
-#   from_port         = 22
-#   protocol          = "tcp"
-#   to_port           = 22
+resource "aws_security_group_rule" "catalogue" {
+  type              = "ingress"
+  security_group_id = local.catalogue_security_group_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
   
-# }
+}
 
-# resource "aws_security_group_rule" "catalogue_egress" {
-#   type              = "egress"
-#   security_group_id = local.mysql_security_group_id
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
+resource "aws_security_group_rule" "catalogue_egress" {
+  type              = "egress"
+  security_group_id = local.mysql_security_group_id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "mongodb_catalogue" {
-#   type              = "ingress"
-#   security_group_id = local.mongodb_security_group_id
-#   source_security_group_id = local.mongodb_security_group_id
-#   from_port         = 27017
-#   protocol          = "tcp"
-#   to_port           = 27017
-# }
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  security_group_id = local.mongodb_security_group_id
+  source_security_group_id = local.mongodb_security_group_id
+  from_port         = 27017
+  protocol          = "tcp"
+  to_port           = 27017
+}
 
-# resource "aws_security_group_rule" "catalogue_backend_alb" {
-#   type              = "ingress"
-#   security_group_id = local.catalogue_security_group_id
-#   source_security_group_id = local.backend_alb_sg_id
-#   from_port         = 8080
-#   protocol          = "tcp"
-#   to_port           = 8080
-# }
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  security_group_id = local.catalogue_security_group_id
+  source_security_group_id = local.backend_alb_sg_id
+  from_port         = 8080
+  protocol          = "tcp"
+  to_port           = 8080
+}
