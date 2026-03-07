ac30d604a56b6e954a7a9372a8f0d0e5de812659
Asri <asri>
2026-03-07 19:54:57 +0530

    roboshop modules

--- 80-frontend-alb/parameter.tf
+++ 80-frontend-alb/parameter.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "frontend_alb_arn_listiner" {
-  name  = "/${var.project_name}/${var.environment}/frontend_alb_arn_listiner"
+resource "aws_ssm_parameter" "frontend_alb_certificate_arn" {
+  name  = "/${var.project_name}/${var.environment}/frontend_alb_certificate_arn"
   type  = "String"
-  value = aws_lb_listener.frontend_https_listener.arn
+  value = aws_acm_certificate.roboshop.arn
 }
