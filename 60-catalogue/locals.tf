264f01a016f179778ef16475135c426447bada30
Asri <asri>
2026-03-07 10:11:09 +0530

    roboshop modules

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,6 @@
 locals {
   catalogue_security_group_id=data.aws_ssm_parameter.catalogue_security_group_id.value
-  private_subnetsg =split("," , data.aws_ssm_parameter.private_subnetsg.value)[0]
+  private_subnetsg =split("," , data.aws_ssm_parameter.private_subnetsg.value)
   vpc_id = data.aws_ssm_parameter.vpc_id.value
 
 }
