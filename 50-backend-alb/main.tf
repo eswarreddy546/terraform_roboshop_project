f22d711c115f9264190070f1f4c855392afa04e9
Asri <asri>
2026-03-07 10:40:24 +0530

    roboshop modules

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -28,18 +28,30 @@ subnets = local.private_subnetsg
 # }
 
 # Create the ALB Listener
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.backendalb.arn
-  port              = "80"
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.backendalb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
+#   default_action {
+#     type = "redirect"
 
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+#     redirect {
+#       port        = "443"
+#       protocol    = "HTTPS"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zoneid
+  name    = "*.backend-alb-${var.environment}-${var.domainname}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.backendalb.dns_name
+    zone_id                = aws_lb.backendalb.zone_id
+    evaluate_target_health = true
   }
 }
--- 50-backend-alb/variable.tf
+++ 50-backend-alb/variable.tf
@@ -6,3 +6,11 @@ variable "environment" {
     default = "dev"
 }
 
+variable "zoneid" {
+  default = "Z00224221HVTOMR4RK7YI"
+
+}
+
+variable "domainname" {
+  default = "eswar.xyz"
+}
